2d3cee0a06b54ff5f790d9dc7923ada06fa1eb0e
css3studio <css3studio@naver.com>
2017-08-28 21:28:03 +0900

    메인화면 검색버튼 텍스트
    검색옵션 선택에 따라 검색버튼의 텍스트 변경되도록 수정

--- assets/js/archive.js
+++ assets/js/archive.js
@@ -8,8 +8,17 @@ var $ = jQuery;
 $(document).ready(function() {
 		
 	//폼요소 스타일링
-  $(".cf01 select").selectmenu();
-	$(".cf02 select").selectmenu();
+  	$(".cf01 select").selectmenu();
+	//$(".cf02 select").selectmenu();
+	$(".cf02 select").selectmenu({
+		change: function( event, data ) {
+			if(data.item.value == "0")
+				$(".cf02 button").text("웹사이트에서 검색");
+			else
+				$(".cf02 button").text("기록에서 검색");
+		}
+	});
+	
 	$(".ng04 select").selectmenu();
 	$(".tkg01").buttonset();
 	//검색창 제어
